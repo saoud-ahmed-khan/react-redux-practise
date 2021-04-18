5d8971ec0556f12ae38873fa16324c633150b1fe
Saoud Ahmed Khan <60979682+saoud-ahmed-khan@users.noreply.github.com>
2021-04-18 15:18:53 +0500

    adding action and reducer

---
+++ src/Redux/Cake/cakeAction.js
@@ -0,0 +1,6 @@
+import { BUY_CAKE } from "./cakeTypes";
+const byCake = () => {
+  return {
+    type: BUY_CAKE,
+  };
+};
---
+++ src/Redux/Cake/cakeReducer.js
@@ -0,0 +1,15 @@
+import { BUY_CAKE } from "./cakeTypes";
+const initialState = {
+  numOfCakes: 10,
+};
+
+export const cakeReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case BUY_CAKE:
+      return { ...state, numOfCakes: state.numOfCakes - 1 };
+
+    default:
+      return state;
+  }
+};
+
